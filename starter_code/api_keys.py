c7abf84e2b4ebea9965821c7567d176d689ce2c5
jakestan28 <66025464+jakestan28@users.noreply.github.com>
2020-11-09 11:13:01 -0500

    last push

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "addc0d6cc30ea3e8f5a39d5d9c9f6551"
+weather_api_key = "48cd2f4c6e1cc4ac15520e0f752a9eb5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDGHMgFpV-31szyZ5-c6SeIHkvIOF_liGY"
